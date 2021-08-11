26f5cf8acc5ff60b8b79c12acee9a93251fca61f
Carlos Eduardo <carlosgpeixoto1@gmail.com>
2021-08-10 23:04:27 -0300

    :package:menu sidebar

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # portifolio
+
 Portifolio-tutorial
 
 ## Tarefas
+
 O Controle das tarefas desse projeto será realizado no github
 
 ## Icones
+
 :package :Novas funcionalidades
 :up: atualização
 :beatle: correção de bug
--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-var hamburguer = document.querySelector(".hamburguer");
-    
-hamburguer.addEventListener("click", function(){
-
-    document.querySelector(".container").classList.toggle("show-menu");
-});
+document.querySelector(".hamburguer").addEventListener("click", ()=>
+    document.querySelector(".container").classList.toggle("show-menu"));
